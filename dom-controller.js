416bc80ff001c01978c96c5e818340310b15f2db
kriot <gosha-bor@yandex.ru>
2014-03-30 18:20:40 +0400

    Select method

--- dom-controller.js
+++ dom-controller.js
@@ -1,17 +1,20 @@
 /* DOM Controller */
 function D(select)
 {
-	return new DOMController(select,document);
+	return (new DOMController()).Select(select,document);
 }
-function DOMController(select, par)
+function DOMController()
 {
-	var firstChar = select.split('')[0];
-	if(firstChar == "#")
-		this.DO = new Array(par.getElementById(select.substring(1)));
-	else if(firstChar == ".")
-		this.DO = par.getElementsByClass(select.substr(1));
-	else
-		this.DO = par.getElementsByTagName(select);
+	this.Select = function(select, par){
+		var firstChar = select.split('')[0];
+		if(firstChar == "#")
+			this.DO = new Array(par.getElementById(select.substring(1)));
+		else if(firstChar == ".")
+			this.DO = par.getElementsByClass(select.substr(1));
+		else
+			this.DO = par.getElementsByTagName(select);
+		return this;
+	}
 	this.setCSS = function(attr, val)
 	{
 		for(var item in this.DO)
@@ -23,5 +26,17 @@ function DOMController(select, par)
 		for(var item in this.DO)
 			if(this.DO.hasOwnProperty(item))
 				return this.DO[item].style[attr]; 
+	}
+	this.setAttr = function(attr, val)
+	{
+		for(var item in this.DO)
+			if(this.DO.hasOwnProperty(item))
+				this.DO[item].setAttribute(attr, val);
+	}
+	this.getAttr = function(attr)
+	{
+		for(var item in this.DO)
+			if(this.DO.hasOwnProperty(item))
+				return this.DO[item].getAttribute(attr); 
 	}
 }
