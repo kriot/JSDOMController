594fd997ef9531d78dba8902b2c1099b42e07735
kriot <gosha-bor@yandex.ru>
2014-03-30 14:17:12 +0100

    select elems, css modifications

--- dom-controller.js
+++ dom-controller.js
@@ -1,1 +1,27 @@
 /* DOM Controller */
+function D(select)
+{
+	return new DOMController(select,document);
+}
+function DOMController(select, par)
+{
+	var firstChar = select.split('')[0];
+	if(firstChar == "#")
+		this.DO = new Array(par.getElementById(select.substring(1)));
+	else if(firstChar == ".")
+		this.DO = par.getElementsByClass(select.substr(1));
+	else
+		this.DO = par.getElementsByTagName(select);
+	this.setCSS = function(attr, val)
+	{
+		for(var item in this.DO)
+			if(this.DO.hasOwnProperty(item))
+				this.DO[item].style[attr] = val;
+	}
+	this.getCSS = function(attr)
+	{
+		for(var item in this.DO)
+			if(this.DO.hasOwnProperty(item))
+				return this.DO[item].style[attr]; 
+	}
+}
--- using.js
+++ using.js
@@ -1,1 +1,8 @@
 /* Using */
+
+addEventListener("DOMContentLoaded", function()
+{
+	console.log("loaded");
+	D("#mydiv").setCSS("backgroundColor", "red");
+	console.log(D("#mydiv").getCSS("height"));
+});
