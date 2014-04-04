6d34a5aa430b48506a8e16a195081f37ed09360a
kriot <gosha-bor@yandex.ru>
2014-04-04 15:40:19 +0400

    test files

--- using.js
+++ using.js
@@ -1,8 +1,21 @@
 /* Using */
 
-addEventListener("DOMContentLoaded", function()
+D(document).on("DOMContentLoaded", function()
 {
 	console.log("loaded");
-	D("#mydiv").setCSS("backgroundColor", "red");
-	console.log(D("#mydiv").getCSS("height"));
+	//D("a").setAttr("href","olo.html");
+	console.log(D("ul").Find("li").Find("a").Parent().Find("span").CreateChild("input").attr("value","text").val());
+	D("li").css("backgroundColor", "green");
+	console.log(D("li").css("backgroundColor"));
+	D("li").attr("type","square");
+	console.log(D("li").attr("type"));
+	D("body > a").html("olo");
+	console.log(D("a").html());
+	D(document.getElementById("myli")).css("color","grey");
+	D(document.body).css("backgroundColor","black");
+	D(document.documentElement).each(function(){
+		alert(this.html());
+	});
+	console.log(document.documentElement.style);
+	D(document.body).CreateChild("a").html("added span").attr("href","#");
 });
